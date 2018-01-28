5e76dddd69ee8fc17644641da3f81a86dc4e501f
tier61rus <tier61rus@mail.ru>
2018-01-29 00:38:27 +0300

    some new tips

--- python_tips.py
+++ python_tips.py
@@ -6,3 +6,9 @@
 y = "Who understand %r and who %s " % (binary, do_not)
 # разделитель
 print "." * 10
+
+#импорт модуля
+from sys import argv
+
+# аргументы на вход
+script_name, weight, height = argv
