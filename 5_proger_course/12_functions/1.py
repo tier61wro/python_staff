2803640331959b02e7af4eae59259f1fdd57ef03
tier61rus <tier61rus@mail.ru>
2018-12-30 18:32:17 +0300

    func ex added

--- 5_proger_course/12_functions/1.py
+++ 5_proger_course/12_functions/1.py
@@ -11,3 +11,25 @@ s = count_distance(1,1);
 back_mess = (lambda a: "kilometr" if a == 1 else "kilometrov")(s)
 
 print ("Вы проедете: ", s, back_mess )
+
+# Создайте функцию, которая будет принимать параметр и внутри этой функции будет еще одна функция, которая также будет принимать параметр. Вторая функция должна просто суммировать оба параметра, а первая должна вернуть результат.
+
+def funk1(par1):
+    def funk2(par2):
+        return par1 + par2
+    return funk2
+
+summa_sotka_funk = funk1(100)
+res = summa_sotka_funk(200)
+print ("res = ", res)
+
+#Создайте функцию с параметрами по умолчанию. Вызовите эту функцию и передайте в нее не все параметры. Функция должна вернуть деление всех чисел. При этом добавьте проверку при деления на ноль.
+
+def chastnoe (a,b,c = 1):
+    if ((a == 0) or (b == 0) or (c == 0 )):
+        print ("alarm")
+    else:
+        return int(a/b/c)
+
+diff = chastnoe (8,4)
+print ("diff=", diff)
