23de9de8a2572d0c8c5719ede3dfad8985d2f6ff
tier61rus <tier61rus@mail.ru>
2019-06-29 15:28:47 +0300

    lesson 3 templates extend

--- 13_django_molchanov/app/blogengine/blog/views.py
+++ 13_django_molchanov/app/blogengine/blog/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def posts_list(request):
-    return HttpResponse("<h1>Hello from post list</h1>")
+    #return HttpResponse("<h1>Hello from post list</h1>")
+    names = ['sasha', 'karo', 'luna']
+    return render(request, 'blog/index.html', context={'names': names})
+    #return render(request, 'blog/base.html', context={'names': names})
